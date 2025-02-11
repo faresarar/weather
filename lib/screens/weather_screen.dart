8fd8c11cd11f032cc31c93ebc138ee9e24c28875
faresarar <faresarar455@gmail.com>
2025-02-11 20:08:54 +0100

    Creating Ui Background and Search Bar

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -10,6 +10,42 @@ class WeatherScreen extends StatefulWidget {
 class _WeatherScreenState extends State<WeatherScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(
+            "assets/images/haze.jpg",
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          Padding(
+            padding: EdgeInsets.all(15),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 40,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "Entre city Name",
+                      suffixIcon: Icon(
+                        Icons.search,
+                      ),
+                      filled: true,
+                      fillColor: Colors.black26,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
